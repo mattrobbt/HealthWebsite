c52e8c0e2e8829c56740568de024a9e02577bab4
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-01-28 12:30:41 +0000

    added basic Google Maps functionality (createMap, addMarker)

---
+++ Software_Code/client/src/maps/maps.js
@@ -0,0 +1,38 @@
+const createMap = () => {
+  return new google.maps.Map(document.getElementById('map'), {
+    zoom: 5,
+    center: {
+      lat: 38,
+      lng: -98
+    }
+  });
+};
+
+/**
+ * map -> google.maps.Map
+ * position -> { lat: x, lng: y }
+ * content -> html to be displayed on click
+ */
+const addMarker = (map, position, content) => {
+  const marker = new google.maps.Marker({
+    map: map,
+    position: position
+  });
+
+  marker.addListener('click', () => {
+    const infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    infoWindow.open(map, marker);
+  });
+};
+
+
+/** calling functions for testing */
+const map = createMap();
+
+map.
+
+const markerContent = `<div><h4>Testing</h4></div>`;
+
+addMarker(map, { lat: 40, lng: -85 }, markerContent);

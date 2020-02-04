5a0fed5207a9262f6f4cb2fb75086575d75d2c70
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-02-04 15:36:34 +0000

    added tests for calculateDistance

--- Software_Code/client/test/distanceTest.js
+++ Software_Code/client/test/distanceTest.js
@@ -1,5 +1,19 @@
 const { assert } = require('chai');
-const degreesToRadians = require('../src/distances');
+const { degreesToRadians, calculateDistance } = require('../src/distances');
+
+// Location fake objects for test use
+const location1 = {
+  lat: 50,
+  lng: 60
+};
+const location2 = {
+  lat: 72,
+  lng: 46
+};
+const location3 = {
+  lat: 70,
+  lng: 50
+};
 
 describe('distances', () => {
   describe('degreesToRadians()', () => {
@@ -45,7 +59,35 @@ describe('distances', () => {
 
   describe('calculateDistance()', () => {
     it('is defined', () => {
-      assert.isDefined(calculateDistance());
+      assert.isDefined(calculateDistance);
+    });
+
+    it('returns a number', () => {
+      const result = calculateDistance(location1, location2);
+      assert.isNumber(result);
+    });
+
+    it('distance from location1 to location 2 is 2545km', () => {
+      const result = calculateDistance(location1, location2);
+      assert.equal(result, 2545);
+    });
+
+    // TODO: rounded to nearest km, can increase accuracy in the future
+    it('distance from location2 to location3 is 265km', () => {
+      const result = calculateDistance(location2, location3);
+      assert.equal(result, 265);
+    });
+
+    it('distance from location1 to location2 = location 2 to location 1', () => {
+      const result1 = calculateDistance(location1, location2);
+      const result2 = calculateDistance(location2, location1);
+
+      assert.equal(result1, result2);
+    });
+
+    it("distances shouldn't be negative (< 0)", () => {
+      const result = calculateDistance(location1, location3);
+      assert.isAtLeast(result, 0);
     });
   });
 });

ef9631aaf1d6daf23e63cc957541539798c1dc3b
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-02-05 12:54:43 +0000

    fixed test describe functions

--- Software_Code/client/test/sortByDistanceTest.js
+++ Software_Code/client/test/sortByDistanceTest.js
@@ -48,33 +48,33 @@ describe('sortByDistance', () => {
     it('should be defined', () => {
       assert.isDefined(sortLocations);
     });
+  });
 
-    describe('calculateDistances', () => {
-      it('should be defined', () => {
-        assert.isDefined(calculateDistances);
-      });
+  describe('calculateDistances', () => {
+    it('should be defined', () => {
+      assert.isDefined(calculateDistances);
+    });
 
-      it('should populate the distances array (length should = locations.length)', () => {
-        distances = calculateDistances(userLocation, locations);
-        assert.equal(distances.length, locations.length);
-      });
+    it('should populate the distances array (length should = locations.length)', () => {
+      distances = calculateDistances(userLocation, locations);
+      assert.equal(distances.length, locations.length);
+    });
 
-      // TODO: add reverse to be able to sort both ways to reverse
-      it('should be sortable in ascending order', () => {
-        distances = calculateDistances(userLocation, locations);
-        distances = sortDistances(distances, true);
-        for (let i = 0; i < distances.length - 1; i++) {
-          assert.isAtMost(distances[i], distances[i + 1]);
-        }
-      });
+    // TODO: add reverse to be able to sort both ways to reverse
+    it('should be sortable in ascending order', () => {
+      distances = calculateDistances(userLocation, locations);
+      distances = sortDistances(distances, true);
+      for (let i = 0; i < distances.length - 1; i++) {
+        assert.isAtMost(distances[i], distances[i + 1]);
+      }
+    });
 
-      it('should be sortable in descending order', () => {
-        distances = calculateDistances(userLocation, locations);
-        distances = sortDistances(distances, false);
-        for (let i = 0; i < distances.length - 1; i++) {
-          assert.isAtLeast(distances[i], distances[i + 1]);
-        }
-      });
+    it('should be sortable in descending order', () => {
+      distances = calculateDistances(userLocation, locations);
+      distances = sortDistances(distances, false);
+      for (let i = 0; i < distances.length - 1; i++) {
+        assert.isAtLeast(distances[i], distances[i + 1]);
+      }
     });
   });
 });
